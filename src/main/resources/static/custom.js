64027e5d7565e8930127c2423d0f598ea9683718
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-09 23:48:38 -0600

    marker and message

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,7 +1,35 @@
-var map;
+let map;
+let coords = {lat: 39.9612, lng: -82.9988};
+let marker;
+
+
+
 function initMap() {
+
+    let contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
+
+    let infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+    
+    let image = {url: '/sword.png', 
+      scaledSize: new google.maps.Size(50, 50)};
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 39.9612, lng: -82.9988},
-        zoom: 10
+        center:coords,
+        zoom: 10,
+        scrollwheel:true
+        
     });
+
+    var marker = new google.maps.Marker({
+        position: coords,
+        map: map,
+        icon: image,
+        animation: google.maps.Animation.BOUNCE
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+     });
 }
