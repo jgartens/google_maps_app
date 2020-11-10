feaa53a388541cedffa2cb3556db843874f3f0ab
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-10 17:35:58 -0600

    Added error handling functionality

--- src/main/java/com/tts/mapsapp/controlller/MapController.java
+++ src/main/java/com/tts/mapsapp/controlller/MapController.java
@@ -1,14 +1,62 @@
 package com.tts.mapsapp.controlller;
 
+import com.tts.mapsapp.models.Location;
+import com.tts.mapsapp.service.MapService;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class MapController{
+public class MapController {
 
-    @GetMapping("/home")
-    public String getDefaultMap(){
-        return "index.html";
+	@Autowired
+	MapService mapService;
+
+	@GetMapping("/")
+	public String getDefaultMap(Model model) {
+
+		model.addAttribute(new Location());
+
+		return "index";
+	}
+
+	@PostMapping("/home")
+	public String getMapForLocation(Location location, Model model) {
+
+        Boolean passed = mapService.addCoordinates(location);
+        if (passed == true){
+            model.addAttribute(location);
+
+        }
+        else {
+            // String errorMsg = ;
+            model.addAttribute("errorMsg", "Address not found");
+        }
+		return "index";
     }
-}
+    
+    @PostMapping("/random")
+    public String getRandomMap(Model model) {
+        Location location = mapService.getRandomCoordinates();
+        model.addAttribute(location);
 
+        return "index";
+
+    }
+
+	// @PostMapping("/random")
+	// public String getRandomLocation(Model model){
+
+	// Location city = mapService.randomCity();
+
+	// model.addAttribute(city);
+
+	// return "index";
+	// }
+
+}
--- src/main/java/com/tts/mapsapp/service/MapService.java
+++ src/main/java/com/tts/mapsapp/service/MapService.java
@@ -1,5 +1,9 @@
 package com.tts.mapsapp.service;
 
+import java.util.Random;
+
+import com.tts.mapsapp.models.GeocodingResponse;
+import com.tts.mapsapp.models.Location;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -7,19 +11,42 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MapService {
-    
-    @Value ("{api_key}")
-    private String apiKey;
 
-    public void addCoordinates(Location location) {
-        String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + 
-            location.getCity() + "," + location.getState() + "&key=" + apiKey;
+	@Value("${api_key}")
+	private String apiKey;
+
+	public boolean addCoordinates(Location location) {
+		String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location.getCity() + ","
+				+ location.getState() + "&key=" + apiKey;
+
+		RestTemplate restTemplate = new RestTemplate();
+		GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
+        
+        if (!response.getResults().isEmpty()) {
+            Location coordinates = response.getResults().get(0).getGeometry().getLocation();
+            location.setLat(coordinates.getLat());
+            location.setLng(coordinates.getLng());
+            return true;
+        }
+
+        else{
+            return false;
+        }
+	}
+
+   
+	public Location getRandomCoordinates() {
+        Random r = new Random();
+        double randLong = -180 + (180 + 180) * r.nextDouble();
+        double randLat = -90 + (90 + 90) * r.nextDouble();
+        
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + randLat + "," + randLong 
+        + "&key=" + apiKey;
 
         RestTemplate restTemplate = new RestTemplate();
-        GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
-        Location coordinates = response.getResults().getGeometry().getLocation();
+		GeocodingResponse response = restTemplate.getForObject(url, GeocodingResponse.class);
+		Location location = response.getResults().get(0).getGeometry().getLocation();
+        return location;
+	}
 
-        location.setLat(coordinates.getLat());
-        location.setLng(coordinates.getLng());
-    }
 }
