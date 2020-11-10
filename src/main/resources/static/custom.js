d00252d64121ac287b0d8eb50ecafbfc1cccaafc
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-10 17:36:19 -0600

    bug fixes

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,12 +1,11 @@
 let map;
-let coords = {lat: 39.9612, lng: -82.9988};
 let marker;
 
 
 
 function initMap() {
 
-    let contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
+    let contentString = '<h2>' + city + ', ' + state + '</h2>';
 
     let infowindow = new google.maps.InfoWindow({
         content: contentString
@@ -18,8 +17,10 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center:coords,
         zoom: 10,
-        scrollwheel:true
-        
+        scrollwheel:false,
+        // mapTypeControlOptions: {
+        //     mapTypeIds: ['roadmap', 'satellite']
+        //  }
     });
 
     var marker = new google.maps.Marker({
@@ -33,3 +34,7 @@ function initMap() {
         infowindow.open(map,marker);
      });
 }
+
+// function changeMapToSatellite(map) {
+//     map.setMapTypeId('satellite');
+// }
